6d09717ac67d9515e95d64ab70ba3de34d0139fa
theoconnetable <56427602+theoconnetable@users.noreply.github.com>
2021-12-13 10:36:31 +0100

    high score marche

--- highscore.py
+++ highscore.py
@@ -1,6 +1,24 @@
 def newhighscore(score,nom):
     '''###########################################################'''
-    ##open score.txt
-    ##if score > suiv de :
+    f = open("highscore.txt")
+    phrase = f.readline()
+    f.close()
+    taille = len(phrase)
+    rang = 0
+    while phrase[rang] != ":" :
+        rang= rang+1
+    ancienscore = ''
+    for i in range(rang + 1,len(phrase)):
+        ancienscore = ancienscore + phrase[i]
+    ancienscore = int(ancienscore)
+    if score > int(ancienscore):
+        newphrase = nom + ' :' + str(score)
+        g = open("highscore.txt","w")
+        g.write(newphrase)
+        g.close()
+        return True
+    else:
+        return False
+    
         ##remplace score
     
