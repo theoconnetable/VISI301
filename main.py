16aaabdfbfbfab838c4d564f8c1b47dcec581809
Emilien39110 <emilien.boitouzet@hotmail.fr>
2021-12-15 21:26:58 +0100

    bonus essaie

--- main.py
+++ main.py
@@ -148,7 +148,15 @@ class Star:
 
     def draw(self, screen):
         screen.blit(self.image2, self.area)
-
+class Bonus :
+    def __init__(self,x2,y2):
+        self.image_sablier = pygame.image.load("sablier.png")
+        self.sablier_rect = self.image_sablier.get_rect(x=x2,y=y2)
+        self.pos =[x2, y2]
+    
+    def draw(self,screen):
+        screen.blit(self.image_sablier, self.sablier_rect)
+        
 class Score:
     ##Le score
     def __init__(self, valscore, screen):
@@ -215,6 +223,7 @@ class Game:
         self.player = Player(200, 700)
         self.star1 = Star(aleatoire(screen.get_width(),self.screen.get_height())[0],aleatoire(screen.get_width(),self.screen.get_height())[1])
         self.star2 = Star(aleatoire(screen.get_width(),self.screen.get_height())[0],aleatoire(screen.get_width(),self.screen.get_height())[1])
+        self.sablier = Bonus(aleatoire(screen.get_width(),self.screen.get_height())[0],aleatoire(screen.get_width(),self.screen.get_height())[1])
         ##On definit les positions initiales du joueur et de l'étoile
         #self.health_max = 200
         self.valscore = 0
@@ -267,8 +276,9 @@ class Game:
             self.particleball.add_particles(self.player.rect.centerx,self.player.rect.centery) #---------------------------------
 
         # Augmentation du score en fonction du score
-        if (self.score.valscore % 20 == 0 and self.health_bar.baisseOk):
+        if (self.score.valscore % 5 == 0 and self.health_bar.baisseOk):
             self.health_bar.augment_baisse()
+            Bonus.draw(self, self.screen)
         if (self.score.valscore % 20 == 1):
             self.health_bar.baisseOk = True
 
